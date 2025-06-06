03c945ff837af0fedf273418541e2bf28bb2771e
Kuntal616 <sadhukhank245@gmail.com>
2025-06-06 09:33:23 +0530

    minor changes

--- Backend/connection.js
+++ Backend/connection.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
     try {
-        const conn = await mongoose.connect(process.env.MongoDB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const conn = await mongoose.connect(process.env.MongoDB_URL);
         console.log("MongoDB Connected");
     } catch (error) {
         console.error(`Error: ${error.message}`);
